eb6912f14605fe39686a1fe5b2710925913d3766
Tomasz Oszczypko <tomekoszczypko@gmail.com>
2023-09-18 20:44:45 +0200

    Added flash effects for damage

--- Scenes/Enemies/enemy.gd
+++ Scenes/Enemies/enemy.gd
@@ -4,10 +4,12 @@ extends CharacterBody2D
 @onready var _sprite := $Sprite
 @onready var _velocity_component := $VelocityComponent as VelocityComponent
 @onready var _health_component := $HealthComponent as HealthComponent
-@onready var _animation_player: AnimationPlayer = $Sprite/AnimationPlayer
+@onready var _animation_player := $Sprite/AnimationPlayer
+@onready var _effects := $Sprite/Effects
 
 
 func _ready() -> void:
+	_health_component.health_changed.connect(_on_health_changed)
 	_health_component.health_depleted.connect(_on_health_depleted)
 
 
@@ -23,6 +25,10 @@ func _handle_animation() -> void:
 	else:
 		_sprite.flip_h = false
 		_animation_player.play("walk_right")
+
+
+func _on_health_changed() -> void:
+	_effects.play("hurt")
 
 
 func _on_health_depleted() -> void:
